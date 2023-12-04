d1b9aa3b2f1d967973cd303d2cb0d599fcb723cc
ericolson1977 <mr_tues_night@yahoo.com>
2023-12-04 09:54:15 -0600

    updated deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,5 +5,3 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googleboo
 module.exports = mongoose.connection;
 
 
-
-mongodb+srv://mrtuesnight:xy8xlNyTPqRwqUWq@cluster0.e8wrs5z.mongodb.net/googlebooks?retryWrites=true&w=majority
